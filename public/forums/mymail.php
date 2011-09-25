745cd33ee6311a0c028a3397e606c3bdc6b5e7fe
Srivathsa Pasumarthi <pv.srivathsa@gmail.com>
2011-09-25 11:33:23 -0700

    forum mail test 3

--- public/forums/mymail.php
+++ public/forums/mymail.php
@@ -1,9 +1,7 @@
 <?php
-	$doc_root =  $_SERVER['DOCUMENT_ROOT'];
-	$doc_root = substr($doc_root,0,strlen($doc_root)-6);
-	$doc_root = $doc_root . "library/Zend";
-	require_once($doc_root . "/Mail.php");
-	require_once($doc_root . "/Mail/Transport/Smtp.php");
+	echo "hey";
+	require_once("/var/www/hive/library/Zend/Mail.php");
+	require_once("/var/www/hive/library/Zend/Mail/Transport/Smtp.php");
 	$config = array('ssl' => "tls", 'port' => 587, 'auth' => "login", 'username' => "admin@hiveusers.com", 'password' => "swordfish");
 	$tr = new Zend_Mail_Transport_Smtp("smtp.gmail.com",$config);
 	Zend_Mail::setDefaultTransport($tr);
